3963e00e4ce938b84cf132c8ca855c8c918ed4ab
Thomas Young <thomas@upcoder.com>
2026-01-19 08:38:26 +0100

    added release 0.1.1 (but just windows archive in this commit)

--- docs/install_mac.sh
+++ docs/install_mac.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 cd ~/Downloads
 
-echo "Downloading Lockstep Arcade 0.1.0..."
-curl -LO https://locksteparcade.com/lockstep_arcade_0.1.0_mac.zip
+echo "Downloading Lockstep Arcade 0.1.1..."
+curl -LO https://locksteparcade.com/lockstep_arcade_0.1.1_mac.zip
 
 echo "Extracting..."
-unzip -o lockstep_arcade_0.1.0_mac.zip -d lockstep_arcade_0.1.0
+unzip -o lockstep_arcade_0.1.1_mac.zip -d lockstep_arcade_0.1.1
 
 echo "Cleaning up..."
-rm lockstep_arcade_0.1.0_mac.zip
+rm lockstep_arcade_0.1.1_mac.zip
 
 echo "Opening folder..."
-open lockstep_arcade_0.1.0
+open lockstep_arcade_0.1.1
 
 echo "Done! Run LockstepArcade to play."
