af7affb012036a256a029acddd612088ae1cd713
Thomas Young <thomas@upcoder.com>
2026-07-16 09:39:40 +0200

    release 1.2.0

--- docs/install_mac.sh
+++ docs/install_mac.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 cd ~/Downloads
 
-echo "Downloading Lockstep Arcade 1.1.1..."
-curl -LO https://locksteparcade.com/lockstep_arcade_1.1.1_mac.zip
+echo "Downloading Lockstep Arcade 1.2.0..."
+curl -LO https://locksteparcade.com/lockstep_arcade_1.2.0_mac.zip
 
 echo "Extracting..."
-unzip -o lockstep_arcade_1.1.1_mac.zip -d lockstep_arcade_1.1.1
+unzip -o lockstep_arcade_1.2.0_mac.zip -d lockstep_arcade_1.2.0
 
 echo "Cleaning up..."
-rm lockstep_arcade_1.1.1_mac.zip
+rm lockstep_arcade_1.2.0_mac.zip
 
 echo "Remove quarantine attribute, if present (needed in case the archive was first downloaded manually)..."
-xattr -d com.apple.quarantine lockstep_arcade_1.1.1/LockstepArcade
+xattr -d com.apple.quarantine lockstep_arcade_1.2.0/LockstepArcade
 
 echo "Opening folder..."
-open lockstep_arcade_1.1.1
+open lockstep_arcade_1.2.0
 
 echo "Done! Run LockstepArcade to play."
